4f1e8f35be03f56e99701abb461c65d4099314d5
Dong Nguyen <nguyenduydong@gmail.com>
2021-08-01 15:52:34 +0700

    working with containers
    - create
    - remove
    - list

--- examples/nats-cluster-bench/docker.go
+++ examples/nats-cluster-bench/docker.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
 )
 
 var cli *client.Client
@@ -48,6 +52,78 @@ func CreateNetwork(s string) error {
 func DeleteNetwork(s string) error {
 	if id, ok := GetNetworkID(s); ok {
 		return cli.NetworkRemove(context.Background(), id)
+	}
+	return nil
+}
+
+func CreateContainer(image, name, port, networkname string, cmds []string) error {
+	containers, err := ListContainer(name)
+	if err != nil {
+		return err
+	}
+	for _, c := range containers {
+		if c.State == "running" {
+			return nil
+		}
+	}
+	if err = RemoveContainer(name); err != nil {
+		return nil
+	}
+	ctx := context.Background()
+	networkid, ok := GetNetworkID(networkname)
+	if !ok {
+		return errors.New(networkname + " is not valid network")
+	}
+	var portset nat.PortSet
+	if len(port) > 0 {
+		portset = nat.PortSet{nat.Port(port): {}}
+	}
+
+	resp, err := cli.ContainerCreate(ctx,
+		&container.Config{
+			Image:        image,
+			Cmd:          cmds,
+			ExposedPorts: portset,
+			Hostname:     name,
+		}, nil, &network.NetworkingConfig{
+			EndpointsConfig: map[string]*network.EndpointSettings{
+				networkname: {
+					NetworkID: networkid,
+				},
+			},
+		}, nil, name)
+	if err != nil {
+		return err
+	}
+
+	return cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{})
+}
+
+func ListContainer(name string) ([]types.Container, error) {
+	ctx := context.Background()
+	return cli.ContainerList(ctx, types.ContainerListOptions{
+		All:     true,
+		Filters: filters.NewArgs(filters.Arg("name", name)),
+	})
+}
+
+func RemoveContainer(name string) error {
+	ctx := context.Background()
+	containers, err := ListContainer(name)
+	if err != nil {
+		return err
+	}
+	for _, c := range containers {
+		if c.State == "running" {
+			if err = cli.ContainerStop(ctx, c.ID, nil); err != nil {
+				return err
+			}
+		}
+		if err = cli.ContainerRemove(ctx, c.ID, types.ContainerRemoveOptions{
+			RemoveVolumes: true,
+		}); err != nil {
+			return err
+		}
 	}
 	return nil
 }
--- examples/nats-cluster-bench/docker_test.go
+++ examples/nats-cluster-bench/docker_test.go
@@ -19,3 +19,15 @@ func TestDeleteNetwork(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestCreateContainer(t *testing.T) {
+	if err := CreateNetwork("testnetwork"); err != nil {
+		t.Fatal(err)
+	}
+	if err := CreateContainer("nats:latest", "test-natsserver", "", "testnetwork", nil); err != nil {
+		t.Fatal(err)
+	}
+	if err := RemoveContainer("test-natsserver"); err != nil {
+		t.Fatal(err)
+	}
+}
